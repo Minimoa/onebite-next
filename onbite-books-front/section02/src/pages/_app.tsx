1c47cc3253976e87dfabd12186cb790102ace38f
Minimoa <12183385@inha.edu>
2025-01-05 21:18:06 +0900

    2.8) 페이지별 레이아웃 설정하기

--- onbite-books-front/section02/src/pages/_app.tsx
+++ onbite-books-front/section02/src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import GlobalLayout from '@/components/global-layout';
-export default function App({ Component, pageProps }: AppProps) {
+import { ReactNode } from 'react';
+import { NextPage } from 'next';
 
-
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode
+}
+export default function App({ Component, pageProps }: AppProps & {
+  Component: NextPageWithLayout
+}) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page)
   return (
     <GlobalLayout>
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </GlobalLayout>
   )
 }
