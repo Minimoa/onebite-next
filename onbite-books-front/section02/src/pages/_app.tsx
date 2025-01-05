d11f7073b788ce581e61e6764fee1d8a6e04f20f
Minimoa <12183385@inha.edu>
2025-01-05 18:16:29 +0900

    2.4) 프리페칭

--- onbite-books-front/section02/src/pages/_app.tsx
+++ onbite-books-front/section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -9,12 +10,17 @@ export default function App({ Component, pageProps }: AppProps) {
   const onClickButton = () => {
     router.push("/test")
   }
+  // 마운트 됐을 때 한번만 test 페이지를 programmatic하게 prefetching
+  useEffect(() => {
+    router.prefetch("/test")
+  }, [])
+
   return (
     <>
       {/* Link 컴포넌트를 이용한 이동 */}
       <Link href={"/"}>index</Link> /
-      <Link href={"/search"}>serach</Link> /
-      <Link href={"/book "}>book</Link> /
+      <Link href={"/search"} prefetch={false}>serach</Link> /
+      <Link href={"/book/1"}>book</Link> /
       {/* programmatic 하게 이동 */}
       <div><button onClick={onClickButton}>/test 페이지로 이동</button></div>
       <Component {...pageProps} />
