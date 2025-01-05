2e1dc04a2f483ef20e3f95f3264ab5bf913e15ee
Minimoa <12183385@inha.edu>
2025-01-05 20:54:43 +0900

    2.7) 글로벌 레이아웃 설정하기

--- onbite-books-front/section02/src/pages/_app.tsx
+++ onbite-books-front/section02/src/pages/_app.tsx
@@ -1,29 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-
+import GlobalLayout from '@/components/global-layout';
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
 
-  const onClickButton = () => {
-    router.push("/test")
-  }
-  // 마운트 됐을 때 한번만 test 페이지를 programmatic하게 prefetching
-  useEffect(() => {
-    router.prefetch("/test")
-  }, [])
 
   return (
-    <>
-      {/* Link 컴포넌트를 이용한 이동 */}
-      <Link href={"/"}>index</Link> /
-      <Link href={"/search"} prefetch={false}>serach</Link> /
-      <Link href={"/book/1"}>book</Link> /
-      {/* programmatic 하게 이동 */}
-      <div><button onClick={onClickButton}>/test 페이지로 이동</button></div>
+    <GlobalLayout>
       <Component {...pageProps} />
-    </>
+    </GlobalLayout>
   )
 }
