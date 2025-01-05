40791b565e8689c4be2873ad2710347caa496ff8
Minimoa <12183385@inha.edu>
2025-01-05 17:45:21 +0900

    feat: page router 실습

--- onbite-books-front/section02/src/pages/_app.tsx
+++ onbite-books-front/section02/src/pages/_app.tsx
@@ -4,7 +4,6 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <h1>글로벌 헤더</h1>
     <Component {...pageProps} />
     </>
   )
---
+++ onbite-books-front/section02/src/pages/search/index.tsx
@@ -0,0 +1,6 @@
+import { useRouter } from 'next/router'
+export default function Page () {
+  const router = useRouter()
+  const { q } = router.query
+  return <h1>serach {q}</h1>
+}
