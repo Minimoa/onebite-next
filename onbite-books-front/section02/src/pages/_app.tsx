6c34bb25a79056951032908395401e869fa22cca
Minimoa <12183385@inha.edu>
2025-01-05 16:35:30 +0900

    feat: create next app project

---
+++ onbite-books-front/section02/src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+    <h1>글로벌 헤더</h1>
+    <Component {...pageProps} />
+    </>
+  )
+}
