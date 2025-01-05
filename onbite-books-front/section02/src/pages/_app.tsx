b46ffb4fcdf45f9f22a41853f256334a9fb5c5f0
Minimoa <12183385@inha.edu>
2025-01-05 17:55:02 +0900

    2.3) 네비게이팅

--- onbite-books-front/section02/src/pages/_app.tsx
+++ onbite-books-front/section02/src/pages/_app.tsx
@@ -1,10 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  const onClickButton = () => {
+    router.push("/test")
+  }
   return (
     <>
-    <Component {...pageProps} />
+      {/* Link 컴포넌트를 이용한 이동 */}
+      <Link href={"/"}>index</Link> /
+      <Link href={"/search"}>serach</Link> /
+      <Link href={"/book "}>book</Link> /
+      {/* programmatic 하게 이동 */}
+      <div><button onClick={onClickButton}>/test 페이지로 이동</button></div>
+      <Component {...pageProps} />
     </>
   )
 }
