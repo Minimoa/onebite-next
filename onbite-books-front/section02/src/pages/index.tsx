e2f8b1d6ef2e116481e74cd375c888f720f4454c
Minimoa <12183385@inha.edu>
2025-01-06 22:40:32 +0900

    2.12) SSR 실습

--- onbite-books-front/section02/src/pages/index.tsx
+++ onbite-books-front/section02/src/pages/index.tsx
@@ -4,37 +4,38 @@ import style from "./index.module.css"
 import SearchableLayout from '@/components/searchable-layout'
 import books from '@/mock/books.json'
 import BookItem from '@/components/book-item'
-import { InferGetServerSidePropsType } from 'next'
+import { InferGetServerSidePropsType, InferGetStaticPropsType } from 'next'
+import fetchBooks from '@/lib/fetch-books'
+import fetchRandomBooks from '@/lib/fetch-randowm-books'
 
 // SSR로 페이지를 동작하게 하기 위한 컨벤션 함수
-export const getServerSideProps = () => {
+export const getServerSideProps = async () => {
   // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
 
-  const data = "hello"
-  console.log('서버사이드프롭스예요')
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ])
   return {
-    props: {
-      data,
+    props: { 
+      allBooks,
+      recoBooks
     }
   }
 }
 
 // InferGetServerSidePropsType는 getServerSideProps의 타입을 추론하는 넥스트 내장 타입
-export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(data)
-  useEffect(() => {
-    console.log(window)
-  }, [])
+export default function Home({allBooks, recoBooks}: InferGetStaticPropsType<typeof getServerSideProps>) {
   return <div className={style.container}>
     <section>
       <h3>지금 추천하는 도서</h3>
-      {books.map((book) => (
+      {recoBooks.map((book) => (
       <BookItem key={book.id} {...book}/>
       ))}
     </section>
     <section>
       <h3>등록된 모든 도서</h3>
-      {books.map((book) => (
+      {allBooks.map((book) => (
       <BookItem key={book.id} {...book}/>
       ))}
     </section>
--- onbite-books-front/section02/src/pages/search/index.tsx
+++ onbite-books-front/section02/src/pages/search/index.tsx
@@ -1,10 +1,21 @@
 import SearchableLayout from '@/components/searchable-layout'
 import { useRouter } from 'next/router'
 import { ReactNode } from 'react'
-import books from '@/mock/books.json'
 import BookItem from '@/components/book-item'
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
+import fetchBooks from '@/lib/fetch-books'
 
-export default function Page () {
+// GetServerSidePropsContext: 현재 브라우저로부터 받은 모든 객체들이 들어 있는 내장 객체
+export const getServerSideProps = async(context: GetServerSidePropsContext) => {
+  const q = context.query.q
+  const books = await fetchBooks(q as string)
+  return {
+    props: {
+      books,
+    }
+  }
+}
+export default function Page ({ books }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return <div>
     {books.map((book) =>
       <BookItem key={book.id} {...book} />
