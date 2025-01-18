ae4c7e432e9f5f2e37deb2851d828fb900cbcd9c
Minimoa <12183385@inha.edu>
2025-01-18 17:24:58 +0900

    section 04 init

---
+++ onbite-books-front/section04/src/components/searchBar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import style from "./serachbar.module.css";
+
+export default function Searchbar() {
+  const router = useRouter(); // App router부터는 라우터 객체에 쿼리 프로퍼티가 제공되지 않음 
+  const searchParams = useSearchParams(); // useSerachParams: 현재 페이지 전달된 쿼리스트링의 값을 가져올 수 있음
+  const [search, setSearch] = useState("");
+
+  const q = searchParams.get("q");
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div className={style.container}>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeyDown}
+      />
+      <button onClick={onSubmit}>검색</button>
+    </div>
+  );
+}
