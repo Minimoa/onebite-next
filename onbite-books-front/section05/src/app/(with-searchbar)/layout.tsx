47746017081529cd838ffb5cb43369fc1d4e8493
Minimoa <alslahdk@gmail.com>
2025-02-04 17:32:03 +0900

    5.2) 풀 라우트 캐시 2

--- onbite-books-front/section05/src/app/(with-searchbar)/layout.tsx
+++ onbite-books-front/section05/src/app/(with-searchbar)/layout.tsx
@@ -1,10 +1,13 @@
 import Searchbar from "@/components/searchBar";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <Searchbar />
+      {/* 사전 렌더링 과정에서 배제 하기 위해서 suspense로 감쌈 */}
+      <Suspense fallback={<div>Loading ...</div>}>
+        <Searchbar />
+      </Suspense>
       {children}
     </div>
   );
