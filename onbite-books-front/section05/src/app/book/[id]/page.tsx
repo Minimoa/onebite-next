adb16bdbad25cf3c76dc2d8a050bd8cebdd7867f
Minimoa <12183385@inha.edu>
2025-02-16 20:18:26 +0900

    5.4) 라우트 세그먼트 옵션

--- onbite-books-front/section05/src/app/book/[id]/page.tsx
+++ onbite-books-front/section05/src/app/book/[id]/page.tsx
@@ -5,6 +5,13 @@ import { notFound } from "next/navigation";
 // StaticParams에서 제공된 params 외에는 생성하지 않도록 하려면 false
 // export const dynamicParams = false
 
+export const dynamic = ''
+// 특정 페이지의 유형을 강제로 Static, Dynamic  페이지로 설정
+// 1. auto : 기본값, 아무것도 강제하지 않음
+// 2. force-dynamic: 페이지를 강제로 dynamic 페이지로 설정 
+// 3. force-static: 페이지를 강제로 static으로 설정
+// 4. error: 페이지를 강제로 Static 페이지로 설정 (설정하면 안되는 이유가 있으면 -> 빌드 오류)
+
 // 빌드 타임에 미리 생성할 페이지 정보를 넘길 수 있음
 export function generateStaticParams () {
   return [{id: '1'}, {id: '2'}, {id: '3'}] // 문자열 데이터로만 명시해야함
