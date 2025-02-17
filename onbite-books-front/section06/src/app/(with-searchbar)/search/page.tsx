5ff98067a1648c8dad37ddac00678d8df14f41e9
Minimoa <alslahdk@gmail.com>
2025-02-17 17:53:43 +0900

    6.3) 스켈레톤 UI 적용하기

--- onbite-books-front/section06/src/app/(with-searchbar)/search/page.tsx
+++ onbite-books-front/section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,4 +1,5 @@
 import BookItem from "@/components/book-item";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
 import { Suspense } from "react";
@@ -28,7 +29,7 @@ export default async function Page({
   }>;
 }) {
   const q = (await searchParams).q
-  return <Suspense key={q || ''} fallback={<div>Loading ...</div>}>
+  return <Suspense key={q || ''} fallback={<BookListSkeleton count={3}/>}>
       <SearchResult q={q || ""}/>
     </Suspense>
 }
